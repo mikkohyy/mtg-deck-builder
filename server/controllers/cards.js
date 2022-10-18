229a26d9f86463263752f8c23b5a9f6ccb48afca
mikko <mikko.e.hyyrylainen@gmail.dom>
2022-10-18 19:27:11 +0300

    refactor #skip-deployment

--- server/controllers/cards.js
+++ server/controllers/cards.js
@@ -7,6 +7,35 @@ const {
 const { extractInformationOnUpdatedObject } = require('../utils/query_handling')
 const { Card } = require('../models')
 
+/**
+ * Object that represents a card that is not in the database
+ * @typedef {Object} NewCard Information about card
+ * @property {number} cardSetId Id of set of cards it belongs to
+ * @property {string} name Name of the card
+ * @property {number} cardNumber Number of the card
+ * @property {string} manaCost Mana cost of the card
+ * @property {float} price Price of the card
+ * @property {string} rulesText Rules text of the card
+ * @property {string} rarity Rarity of the card
+ */
+
+/**
+ * Object that represents a card that exists in the database
+ * @typedef {Object} Card Information about card
+ * @property {number} id Id of the card
+ * @property {number} cardSetId Id of set of cards it belongs to
+ * @property {string} name Name of the card
+ * @property {number} cardNumber Number of the card
+ * @property {string} manaCost Mana cost of the card
+ * @property {float} price Price of the card
+ * @property {string} rulesText Rules text of the card
+ * @property {string} rarity Rarity of the card
+ */
+
+/**
+ * Endpoint for getting a card which id is defined as paremeter
+ * @returns {Card} Card defined by the id
+ */
 cardsRouter.get('/:id', validateIdWhichIsInteger, async(request, response, next) => {
   const cardId = request.params.id
 
@@ -17,6 +46,12 @@ cardsRouter.get('/:id', validateIdWhichIsInteger, async(request, response, next)
     next(error)
   }
 })
+
+/**
+ * Endpoint for adding a new card
+ * @property {NewCard} Card Card that user wants to add in the body of the request
+ * @returns {Card} The created card in the body of the response
+ */
 
 cardsRouter.post('/', validateCardObjectAddedToCardSet, async(request, response, next) => {
   try {
@@ -29,20 +64,23 @@ cardsRouter.post('/', validateCardObjectAddedToCardSet, async(request, response,
   }
 })
 
+/**
+ * Endpoint for updating a card
+ * @property {Card} Card Card that user wants to update in the body of the request
+ * @returns {Card} Updated card in the body of the response
+ */
 cardsRouter.put(
   '/:id',
   validateIdWhichIsInteger,
   validateExistingCardObject,
   async (request, response, next) => {
-    let updatedObject = null
-
     try {
       const cardId = request.params.id
       const card = request.body
 
       delete card.id
 
-      const updatedInfo = await Card.update(
+      const updateInfo = await Card.update(
         card,
         {
           where: { id: cardId },
@@ -50,11 +88,7 @@ cardsRouter.put(
         }
       )
 
-      const updatedRows = updatedInfo[0]
-
-      if (updatedRows) {
-        updatedObject = extractInformationOnUpdatedObject(updatedInfo)
-      }
+      const updatedObject = extractUpdatedObject(updateInfo)
 
       updatedObject ? response.json(updatedObject) : response.status(404).end()
     } catch(error) {
@@ -63,6 +97,9 @@ cardsRouter.put(
   }
 )
 
+/**
+ * Endpoint for deleting a card that is defined in the parameters
+ */
 cardsRouter.delete('/:id', validateIdWhichIsInteger, async (request, response) => {
   const card = await Card.findByPk(request.params.id)
 
@@ -73,5 +110,44 @@ cardsRouter.delete('/:id', validateIdWhichIsInteger, async (request, response) =
     response.status(404).end()
   }
 })
+
+/**
+ * @param {object} updateInfo Response for update request
+ * @returns {object|null} If update was succesful returns updated object, otherwise null
+ */
+const extractUpdatedObject = (updateInfo) => {
+  let updatedObject = null
+
+  if (cardWasUpdated(updateInfo)) {
+    updatedObject = extractInformationOnUpdatedObject(updateInfo)
+  }
+
+  return updatedObject
+}
+
+/**
+ * @param {object} updateInfo Response for update request
+ * @returns {boolean} Returns true if a card was updated
+ */
+
+const cardWasUpdated = (updateInfo) => {
+  let wasUpdated = false
+  const updatedRows = getChangedRowsN(updateInfo)
+
+  if (updatedRows) {
+    wasUpdated = true
+  }
+
+  return wasUpdated
+}
+
+/**
+ * @param {object} updateInfo Response for update request
+ * @returns {number} Number of updated rows
+ */
+
+const getChangedRowsN = (updateInfo) => {
+  return updateInfo[0]
+}
 
 module.exports = cardsRouter
