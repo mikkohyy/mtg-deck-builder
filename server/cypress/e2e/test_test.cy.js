20d672b46b511ffc30d9f50ab8e06cb041148673
mikko <mikko.e.hyyrylainen@gmail.dom>
2022-11-07 22:25:53 +0200

    remove old 2e2 tests

--- server/cypress/e2e/test_test.cy.js
+++ server/cypress/e2e/test_test.cy.js
@@ -1,17 +1,21 @@
-const { PORT } = require('../../utils/config')
-const serverAddress = `http://localhost:${PORT}`
+// const { PORT } = require('../../utils/config')
+// const serverAddress = `http://localhost:${PORT}`
 
 describe('tests that testing works', () => {
+  /*
   it('visits the page', function() {
     cy.request('POST', `${serverAddress}/api/initialize-e2e`)
     cy.visit(`${serverAddress}/`)
-    cy.contains('Hello world!')
+    cy.contains('Logout')
   })
+  */
 
+  /*
   it('button pressing works', function() {
     cy.request('POST', `${serverAddress}/api/initialize-e2e`)
     cy.visit(`${serverAddress}/`)
     cy.contains('Press me').click()
     cy.contains('1 - eka')
   })
+  */
 })
